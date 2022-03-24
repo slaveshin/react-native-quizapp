d0124ea479326aca1c5e3a01f69db0f8c670ad6c
신승재 <sinseungjae@sinseungjaeui-MacBookPro.local>
2022-03-24 17:00:11 +0900

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from "react";
+import { Animated, Easing, Pressable, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.View`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
+  const [up, setUp] = useState(false);
+  const Y_POSITION = useRef(new Animated.Value(0)).current;
+  const toggleUp = () => setUp((prev) => !prev);
+  const moveUp = () => {
+    Animated.timing(Y_POSITION, {
+      toValue: up ? 300 : -300,
+      useNativeDriver: true, // 브리지를 통과 할 필요없음
+      easing: Easing.cubic,
+      duration: 3000,
+    }).start(toggleUp);
+  };
+  const opacity = Y_POSITION.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0.5, 1],
+  });
+  const borderRadius = Y_POSITION.interpolate({
+    inputRange: [-300, 300],
+    outputRange: [100, 0],
+  });
+  Y_POSITION.addListener(() => {
+    console.log("Y VALUE:", Y_POSITION);
+    console.log("opacity VALUE:", opacity);
+    console.log("borderRadius VALUE:", borderRadius);
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Pressable onPress={moveUp}>
+        <AnimatedBox
+          style={{
+            borderRadius,
+            opacity,
+            transform: [{ translateY: Y_POSITION }],
+          }}
+        />
+      </Pressable>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
